5aceae056691ca348d15ebb0a567e716341c3d89
Yashika dixit <dixityashika606@gmail.com>
2026-03-24 09:05:35 +0530

    Add student dictionary with details and print statements

---
+++ dictionary1.py
@@ -0,0 +1,13 @@
+student = {
+    "name": "Yashika",
+    "age": 20,
+    "course": "BCA",
+    "marks": {
+        "Python": 90,
+        "Java": 85,
+        "Math": 88
+    }
+}
+
+print("Name:", student["name"])
+print("Python Marks:", student["marks"]["Python"])
