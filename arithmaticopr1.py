c94f20bd982c4da1c14f2a7a8652118d2099dd0a
Yashika dixit <dixityashika606@gmail.com>
2026-03-24 09:11:53 +0530

    Add basic arithmetic operations in arithmaticopr1.py

---
+++ arithmaticopr1.py
@@ -0,0 +1,21 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Division by zero not allowed"
+    return a / b
+
+a = 20
+b = 5
+
+print("Addition:", add(a, b))
+print("Subtraction:", subtract(a, b))
+print("Multiplication:", multiply(a, b))
+print("Division:", divide(a, b))
